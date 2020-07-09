146b43872d2cc1d8def311f9ed18877f871ec7c7
Aykut Sarac <61199137+aykut-sarac@users.noreply.github.com>
2020-07-09 16:36:55 +1000

    Add files via upload

---
+++ component---src-pages-404-js-a28c96197bc67fe85896.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,a,n){"use strict";n.r(a);var t=n("q1tI"),l=n.n(t),r=n("7oih"),o=n("Wbzz");n("EYWl");a.default=function(){return l.a.createElement(r.a,null,l.a.createElement("main",{className:"error-page"},l.a.createElement("div",{className:"error-container"},l.a.createElement("h1",null,"What you looking for?:( ",l.a.createElement("br",null)," I am not google:("),l.a.createElement(o.a,{to:"/",className:"btn"},"back home"))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a28c96197bc67fe85896.js.map
